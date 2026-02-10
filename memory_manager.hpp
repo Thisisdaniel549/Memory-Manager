8950f363550a97279af379383481d6aeb9ef17c5
Daniel <valorantfan82@gmail.com>
2026-02-10 18:28:17 -0500

    Small modifications to definitions
    Updated definitions to use const, and adjusted the definitions to match the new ones in the memory_manager.cpp

--- memory_manager.hpp
+++ memory_manager.hpp
@@ -16,16 +16,16 @@ public:
 	std::string module_name;
 
 	bool initialize();
-	uintptr_t get_base(std::string process_name);
-	uintptr_t get_base_size(std::string process_name);
-	uintptr_t get_process_id(std::string process_name);
+	uintptr_t get_base(const std::string& process_name);
+	uintptr_t get_base_size(const std::string& process_name);
+	uintptr_t get_process_id(const std::string& process_name);
 
 	template<typename X>
 	X read(uintptr_t address);
 
 	template<typename X>
-	X write(uintptr_t address, X* value);
+	void write(uintptr_t address, const X& value);
 
 	bool find_pattern(const char* pattern, const char* mask, uintptr_t& out_address);
 };
-inline std::unique_ptr<c_memory_manager> g_memory = std::make_unique<c_memory_manager>();
+inline std::unique_ptr<c_memory_manager> memory = std::make_unique<c_memory_manager>();
