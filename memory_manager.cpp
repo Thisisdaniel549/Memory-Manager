d2f0da93c501cfc2f40422ecfe9ae62bbc6bd4dd
Daniel <valorantfan82@gmail.com>
2026-02-10 18:27:07 -0500

    Small Fixes
    Redid methods, fixed NtReadVirtualMemory and NtWriteVirtualMemory not working, Fixed crashing with the find_pattern function.

--- memory_manager.cpp
+++ memory_manager.cpp
@@ -1,65 +1,95 @@
-#include "memory_manager.hpp"
+#include "memory.hpp"
+#include "../logging/logging.hpp"
+
+#ifndef NT_SUCCESS
+#define NT_SUCCESS(Status) (((NTSTATUS)(Status)) >= 0)
+#endif
 
 typedef NTSTATUS(WINAPI* pNtReadVirtualMemory)(HANDLE ProcessHandle, PVOID BaseAddress, PVOID Buffer, ULONG NumberOfBytesToRead, PULONG NumberOfBytesRead);
 typedef NTSTATUS(WINAPI* pNtWriteVirtualMemory)(HANDLE Processhandle, PVOID BaseAddress, PVOID Buffer, ULONG NumberOfBytesToWrite, PULONG NumberOfBytesWritten);
 
-pNtReadVirtualMemory* NtReadVirtualMemory;
-pNtWriteVirtualMemory* NtWriteVirtualMemory;
+pNtReadVirtualMemory NtReadVirtualMemory = nullptr;
+pNtWriteVirtualMemory NtWriteVirtualMemory = nullptr;
 
 bool c_memory_manager::initialize() {
-	this->process_name = "FiveM.exe";
-	this->module_name = "FiveM.exe";
+	HMODULE ntdll = GetModuleHandleA("ntdll.dll");
+	NtReadVirtualMemory = (pNtReadVirtualMemory)GetProcAddress(ntdll, "NtReadVirtualMemory");
+	NtWriteVirtualMemory = (pNtWriteVirtualMemory)GetProcAddress(ntdll, "NtWriteVirtualMemory");
+	
+	if (!NtReadVirtualMemory || !NtWriteVirtualMemory) {
+		logging::error("failed to get NtReadVirtualMemory or NtWriteVirtualMemory");
+		return false;
+	}
+
+	this->process_name = "csgo.exe";
+	this->module_name = "csgo.exe";
 	this->base_module = this->get_base(this->module_name);
 	this->base_size = this->get_base_size(this->module_name);
+	if (!this->base_module || !this->base_size) {
+		logging::error("failed to get base module or base size for %s", this->module_name.c_str());
+		return false;
+	}
+
 	this->process_id = this->get_process_id(this->process_name);
+	if (!this->process_id) {
+		logging::error("failed to get process id for %s", this->process_name.c_str());
+		return false;
+	}
+
 	this->handle = OpenProcess(PROCESS_ALL_ACCESS, TRUE, (DWORD)this->process_id);
+	if (!this->handle) {
+		logging::error("failed to open process with id %d", this->process_id);
+	    return false;
+	}
 
 	return true;
 }
 
-uintptr_t c_memory_manager::get_base(std::string process_name) {
-	uintptr_t addr = NULL;
+uintptr_t c_memory_manager::get_base(const std::string& module_name) {
+	HANDLE snap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, this->process_id);
 
-	if (!this->process_id)
-		return NULL;
+	if (snap == INVALID_HANDLE_VALUE)
+		return 0;
 
-	HANDLE hsnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, (DWORD)process_id);
-	if (hsnap == INVALID_HANDLE_VALUE)
-		return NULL;
+	MODULEENTRY32 me{};
+	me.dwSize = sizeof(me);
 
-	MODULEENTRY32 me = { 0 };
-
-	me.dwSize = sizeof(MODULEENTRY32);
-	if (Module32First(hsnap, &me)) {
-		this->base_module = (uintptr_t)me.modBaseAddr;
+	if (Module32First(snap, &me)) {
+		do {
+			if (!_stricmp(me.szModule, module_name.c_str())) {
+				CloseHandle(snap);
+				return (uintptr_t)me.modBaseAddr;
+			}
+		} while (Module32Next(snap, &me));
 	}
-	CloseHandle(hsnap);
 
-	return this->base_module;
+	CloseHandle(snap);
+	return 0;
 }
 
-uintptr_t c_memory_manager::get_base_size(std::string process_name) {
-	uintptr_t addr = NULL;
+uintptr_t c_memory_manager::get_base_size(const std::string& module_name) {
+	HANDLE snap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, this->process_id);
 
-	if (!this->process_id)
-		return NULL;
+	if (snap == INVALID_HANDLE_VALUE)
+		return 0;
 
-	HANDLE hsnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, (DWORD)process_id);
-	if (hsnap == INVALID_HANDLE_VALUE)
-		return NULL;
+	MODULEENTRY32 me{};
+	me.dwSize = sizeof(me);
 
-	MODULEENTRY32 me = { 0 };
-
-	me.dwSize = sizeof(MODULEENTRY32);
-	if (Module32First(hsnap, &me)) {
-		this->base_size = (uintptr_t)me.modBaseSize;
+	if (Module32First(snap, &me)) {
+		do {
+			if (!_stricmp(me.szModule, module_name.c_str())) {
+				CloseHandle(snap);
+				return (uintptr_t)me.modBaseSize;
+			}
+		} while (Module32Next(snap, &me));
 	}
-	CloseHandle(hsnap);
 
-	return this->base_size;
+	CloseHandle(snap);
+	return 0;
 }
 
-uintptr_t c_memory_manager::get_process_id(std::string process_name) {
+uintptr_t c_memory_manager::get_process_id(const std::string& process_name) {
 	PROCESSENTRY32 pt;
 	HANDLE hsnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 	pt.dwSize = sizeof(PROCESSENTRY32);
@@ -85,30 +115,43 @@ X c_memory_manager::read(uintptr_t address) {
 }
 
 template<typename X>
-X c_memory_manager::write(uintptr_t address, X* value) {
-	X buffer{};
-
-	NtWriteVirtualMemory(this->handle, (PVOID)address, value, sizeof(X), nullptr);
-	return buffer;
+void c_memory_manager::write(uintptr_t address, const X& value) {
+	NtWriteVirtualMemory(this->handle, (PVOID)address, (PVOID)&value, sizeof(X), nullptr);
 }
 
 bool c_memory_manager::find_pattern(const char* pattern, const char* mask, uintptr_t& out_address) {
-    uintptr_t start_address = this->base_module;
+	uintptr_t start_address = this->base_module;
 	uintptr_t end_address = this->base_module + this->base_size;
 	size_t pattern_length = strlen(mask);
 
-	for (uintptr_t current_address = start_address; current_address < end_address - pattern_length; ++current_address) {
+	for (uintptr_t current_address = start_address; current_address <= end_address - pattern_length; ++current_address)
+	{
 		bool found = true;
+
 		for (size_t i = 0; i < pattern_length; ++i) {
-			if (mask[i] != '?' && pattern[i] != *(char*)(current_address + i)) {
+			if (mask[i] == '?')
+				continue;
+
+			char byte = 0;
+			NTSTATUS status = NtReadVirtualMemory(
+				this->handle,
+				(PVOID)(current_address + i),
+				&byte,
+				sizeof(byte),
+				nullptr
+			);
+
+			if (!NT_SUCCESS(status) || byte != pattern[i]) {
 				found = false;
 				break;
 			}
 		}
+
 		if (found) {
 			out_address = current_address;
 			return true;
 		}
 	}
+
 	return false;
 }
